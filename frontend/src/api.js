a76c7bcb8a48e771d88e9e0a196410e8d8c82c00
Naveen Sajeendran <naveen.sajeendran@gmail.com>
2026-01-30 11:34:38 -0500

    Add console logs to debug Supabase env variables

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://hbkjtwavvacaieekxlyy.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_b3KlExKMVWCmjYyW47qFzw_sMb2zrCg";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+console.log("SUPABASE_URL:", SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY:", SUPABASE_ANON_KEY);
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
